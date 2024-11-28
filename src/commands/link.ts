cddb65e95cd85fad98343bc16232b228e8601187
Drew Daniels <drew.daniels@kipuhealth.com>
2024-11-28 11:13:35 -0600

    fix auto-fixable eslint issue, remove todo

--- src/commands/link.ts
+++ src/commands/link.ts
@@ -1,8 +1,8 @@
-import {Args, Command, Flags} from '@oclif/core'
+import { Args, Command, Flags } from '@oclif/core'
 
 export default class Link extends Command {
   static override args = {
-    id: Args.string({description: 'Jira Issue ID'}),
+    id: Args.string({ description: 'Jira Issue ID' }),
   }
 
   static override description = 'Copies Jira Issue Link'
@@ -10,12 +10,11 @@ export default class Link extends Command {
   static override examples = ['<%= config.bin %> <%= command.id %>']
 
   static override flags = {
-    // TODO: Figure out how to get conform to automatically fix eslint problems when NOT using flat config
-    clipboard: Flags.boolean({char: 'f', description: 'Copy to clipboard', default: false}),
+    clipboard: Flags.boolean({ char: 'f', default: false, description: 'Copy to clipboard' }),
   }
 
   public async run(): Promise<void> {
-    const {args, flags} = await this.parse(Link)
+    const { args, flags } = await this.parse(Link)
 
     const name = flags.name ?? 'world'
     this.log(`hello ${name} from /Users/drew.daniels/projects/jg/src/commands/link.ts`)
