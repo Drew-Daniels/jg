dac1295b805434e59c2a6610a7d9b532d2a7bdff
Drew Daniels <drew.daniels@kipuhealth.com>
2024-12-01 12:45:04 -0600

    use unique identifiers

--- test/commands/id/index.test.ts
+++ test/commands/id/index.test.ts
@@ -6,12 +6,12 @@ import utils from '../../../src/utils/index.js'
 
 describe('id', () => {
   beforeEach(() => {
-    sinon.stub(utils, 'getJiraIssueKeyFromCurrentBranch').resolves('EMR-11111')
+    sinon.stub(utils, 'getJiraIssueKeyFromCurrentBranch').resolves('EMR-54321')
     sinon.stub(utils, 'pbcopy')
   })
 
   it('runs id cmd', async () => {
     const { stdout } = await runCommand('id')
-    expect(stdout).to.equal('11111\n')
+    expect(stdout).to.equal('54321\n')
   })
 })
--- test/commands/key/index.test.ts
+++ test/commands/key/index.test.ts
@@ -6,12 +6,12 @@ import utils from '../../../src/utils/index.js'
 
 describe('key', () => {
   beforeEach(() => {
-    sinon.stub(utils, 'getJiraIssueKeyFromCurrentBranch').resolves('EMR-11111')
+    sinon.stub(utils, 'getJiraIssueKeyFromCurrentBranch').resolves('EMR-12345')
     sinon.stub(utils, 'pbcopy')
   })
 
   it('runs key cmd', async () => {
     const { stdout } = await runCommand('key')
-    expect(stdout).to.equal('EMR-11111\n')
+    expect(stdout).to.equal('EMR-12345\n')
   })
 })
