f708e2a8b9507c91bbfa2c0116aacc245af7b8f7
Drew Daniels <drew.daniels@kipuhealth.com>
2024-12-01 11:50:34 -0600

    delete old link command files

--- test/commands/link/index.test.ts
+++ test/commands/link/index.test.ts
@@ -4,27 +4,16 @@ import sinon from 'sinon'
 
 import utils from '../../../src/utils/index.js'
 
-before(() => {
-  const fakeUtils = {
-    async getJiraIssueKeyFromCurrentBranch() {
-      return 'EMR-11111';
-    },
-    async getJiraIssueLink() {
-      return 'https://jira.atlassian.com/browse/EMR-11111';
-    },
-    pbcopy() { }
-  }
-  sinon.replace(utils, 'getJiraIssueKeyFromCurrentBranch', fakeUtils.getJiraIssueKeyFromCurrentBranch)
-  sinon.replace(utils, 'getJiraIssueLink', fakeUtils.getJiraIssueLink)
-  sinon.replace(utils, 'pbcopy', fakeUtils.pbcopy)
-
-})
-
-after(() => {
-  sinon.restore()
-})
-
 describe('link', () => {
+  beforeEach(() => {
+    sinon.stub(utils, 'getJiraIssueKeyFromCurrentBranch').resolves('EMR-11111')
+    sinon.stub(utils, 'getJiraIssueLink').resolves('https://jira.atlassian.com/browse/EMR-11111')
+    sinon.stub(utils, 'pbcopy')
+  })
+  afterEach(() => {
+    sinon.restore()
+  })
+
   it('runs link cmd', async () => {
     const { stdout } = await runCommand('link')
     expect(stdout).to.equal('https://jira.atlassian.com/browse/EMR-11111\n')
