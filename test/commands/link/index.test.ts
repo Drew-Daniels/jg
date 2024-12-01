3e508d0ecf8658e411fc216bd23c3d92ca2ea68c
Drew Daniels <drew.daniels@kipuhealth.com>
2024-12-01 11:22:18 -0600

    move link command into folder

---
+++ test/commands/link/index.test.ts
@@ -0,0 +1,32 @@
+import { runCommand } from '@oclif/test'
+import { expect } from 'chai'
+import sinon from 'sinon'
+
+import utils from '../../../src/utils/index.js'
+
+before(() => {
+  const fakeUtils = {
+    async getJiraIssueKeyFromCurrentBranch() {
+      return 'EMR-11111';
+    },
+    async getJiraIssueLink() {
+      return 'https://jira.atlassian.com/browse/EMR-11111';
+    },
+    pbcopy() { }
+  }
+  sinon.replace(utils, 'getJiraIssueKeyFromCurrentBranch', fakeUtils.getJiraIssueKeyFromCurrentBranch)
+  sinon.replace(utils, 'getJiraIssueLink', fakeUtils.getJiraIssueLink)
+  sinon.replace(utils, 'pbcopy', fakeUtils.pbcopy)
+
+})
+
+after(() => {
+  sinon.restore()
+})
+
+describe('link', () => {
+  it('runs link cmd', async () => {
+    const { stdout } = await runCommand('link')
+    expect(stdout).to.contain('https://jira.atlassian.com/browse/EMR-11111')
+  })
+})
