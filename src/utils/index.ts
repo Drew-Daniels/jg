d53615aac220384312aab5b2141defea9188b0c4
Drew Daniels <drew.daniels@kipuhealth.com>
2024-12-01 17:09:09 -0600

    add 'fetchIssue' function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,7 @@
 import { spawn } from 'node:child_process'
 
 export default {
+  fetchIssue,
   getJiraIssueKeyFromCurrentBranch,
   getJiraIssueLink,
   getLatestPrForJiraIssue,
@@ -8,7 +9,7 @@ export default {
   runShellCmd
 }
 
-function runShellCmd(cmd: string, args: string[] = []): Promise<string> {
+function runShellCmd(cmd: string, args: string[] = []): Promise<Record<string, unknown> | string> {
   return new Promise((resolve, reject) => {
     const child = spawn(cmd, args, { shell: true, stdio: 'pipe' })
 
@@ -38,12 +39,12 @@ function pbcopy(data: string) {
  * @returns Jira Issue Link
  */
 async function getJiraIssueLink(identifier: string): Promise<string> {
-  const link = await runShellCmd(`jira open ${identifier} -n | tr -d '\n'`)
+  const link = await runShellCmd(`jira open ${identifier} -n | tr -d '\n'`) as string
   return link
 }
 
 async function getJiraIssueKeyFromCurrentBranch(): Promise<string> {
-  const jiraIssueKey = await runShellCmd('git branch --show-current | cut -d / -f2- | cut -d / -f1 | tr -d "[:space:]" | tr a-z A-Z')
+  const jiraIssueKey = await runShellCmd('git branch --show-current | cut -d / -f2- | cut -d / -f1 | tr -d "[:space:]" | tr a-z A-Z') as string
   let jiraIssueId;
   // TODO: Check if Jira Issue Key includes prefix
   if (/^[A-Z]+-\d{5,}$/.test(jiraIssueKey)) {
@@ -56,6 +57,11 @@ async function getJiraIssueKeyFromCurrentBranch(): Promise<string> {
 }
 
 async function getLatestPrForJiraIssue(jiraIssueId: string): Promise<string> {
-  const pr = await runShellCmd(`gh search prs ${jiraIssueId} --assignee="@me" --json=number,title,url --match=title --limit=1 | jq -r '.[0] | [.number, .url] | join(" ")'`)
+  const pr = await runShellCmd(`gh search prs ${jiraIssueId} --assignee="@me" --json=number,title,url --match=title --limit=1 | jq -r '.[0] | [.number, .url] | join(" ")'`) as string
   return pr
+}
+
+async function fetchIssue(id: string): Promise<Record<string, unknown>> {
+  const issue = await runShellCmd(`gh issue view ${id}`) as Record<string, unknown>
+  return issue
 }
