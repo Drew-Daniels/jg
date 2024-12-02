708b535a1ec41bcbd59dce6842c3a5994aa21e6a
Drew Daniels <drew.daniels@kipuhealth.com>
2024-12-02 16:29:11 -0600

    squash

--- test/commands/cc/index.test.ts
+++ test/commands/cc/index.test.ts
@@ -1,9 +1,47 @@
 import { runCommand } from '@oclif/test'
 import { expect } from 'chai'
+import sinon from 'sinon'
+
+import utils from '../../../src/utils/index.js'
 
 describe('cc', () => {
-  it('runs cc cmd', async () => {
-    const { stdout } = await runCommand('cc')
-    expect(stdout).to.contain('hello world')
+  let copyToClipboardStub: sinon.SinonStub
+  let getExtractedIssueDataStub: sinon.SinonStub
+  beforeEach(() => {
+    sinon.stub(utils, 'getJiraIssueKeyFromCurrentBranch').resolves('EMR-11111')
+    copyToClipboardStub = sinon.stub(utils, 'copyToClipboard')
+  })
+  describe('when run without arguments or options', () => {
+    describe('and on a branch with a Jira Issue Key for a Story', () => {
+      beforeEach(() => {
+        getExtractedIssueDataStub = sinon.stub(utils, 'getExtractedIssueData').resolves({ scopes: [], summary: 'This is an issue summary', type: 'Story' })
+      })
+      it('prints a Conventional Git Commit Message for Jira issue with no scopes', async () => {
+        const { stdout } = await runCommand('cc')
+        expect(stdout).to.equal("feat: This is an issue summary\n")
+      })
+    })
+    describe('and on a branch with a Jira Issue Key for a Bug', () => {
+      beforeEach(() => {
+        getExtractedIssueDataStub = sinon.stub(utils, 'getExtractedIssueData').resolves({ scopes: [], summary: 'This is an issue summary', type: 'Bug' })
+      })
+      it('prints a Conventional Git Commit Message for Jira issue with no scopes', async () => {
+        const { stdout } = await runCommand('cc')
+        expect(stdout).to.equal("fix: This is an issue summary\n")
+      })
+    })
+    describe('and not on a branch with a Jira Issue Key', () => {
+      it('throws an error', async () => {
+        const { error } = await runCommand('cc')
+        expect(error?.message).to.equal('')
+      })
+    })
+
+    describe('and not in a git repository', () => {
+      it('throws an error', async () => {
+        const { error } = await runCommand('cc')
+        expect(error?.message).to.equal('')
+      })
+    })
   })
 })
