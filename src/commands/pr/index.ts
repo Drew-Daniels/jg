270044f581327bcd3f6f8b453536622661eaed9f
Drew Daniels <drew.daniels@kipuhealth.com>
2024-12-02 20:45:40 -0600

    set pr command to inherit from JgCommand

--- src/commands/pr/index.ts
+++ src/commands/pr/index.ts
@@ -1,28 +1,14 @@
-import { Args, Command, Flags } from '@oclif/core'
+import { Args } from '@oclif/core'
 
+import { JgCommand } from '../../jg-command.js'
 import utils from '../../utils/index.js'
 
-export default class Pr extends Command {
+export default class Pr extends JgCommand<typeof Pr> {
   static override args = {
     issueIdOrKey: Args.string({ description: 'file to read' }),
   }
 
   static override description = 'Generates a Slack Message with a Link to a Jira Issue and corresponding GitHub link'
-
-  static override enableJsonFlag = true
-
-  static override examples = [
-    '<%= config.bin %> <%= command.id %>',
-    '<%= config.bin %> <%= command.id %> --clipboard',
-    '<%= config.bin %> <%= command.id %> --clipboard --quiet',
-    '<%= config.bin %> <%= command.id %> --json',
-  ]
-
-  static override flags = {
-    clipboard: Flags.boolean({ char: 'c', default: false, description: 'Copy to clipboard' }),
-    help: Flags.help({ char: 'h', description: 'Show help' }),
-    quiet: Flags.boolean({ char: 'q', dependsOn: ['clipboard'], description: 'Suppress output' }),
-  }
 
   public async run(): Promise<{ message: string }> {
     const { args, flags } = await this.parse(Pr)
@@ -36,6 +22,7 @@ export default class Pr extends Command {
     const jiraIssueLink = await utils.getJiraIssueLink(issueKey)
     const jiraIssueMarkdownLink = `[${issueKey}](${jiraIssueLink})`
 
+    // TODO: Fix issue where a partial message is returned with Jira issue markdown link and empty Gh PR issue link (happens when there is a PR for a jira issue but it is now closed)
     const [ghPrNumber, ghPrLink] = await utils.getLatestPrForJiraIssue(issueKey)
     const ghPrMarkdownLink = `[#${ghPrNumber}](${ghPrLink})`
 
