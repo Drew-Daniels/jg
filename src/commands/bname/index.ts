9743681539463ae613bce2f01b243722c4cdda71
Drew Daniels <drew.daniels@kipuhealth.com>
2024-12-02 20:40:46 -0600

    add todos, set bname function to inherit from JgCommand

--- src/commands/bname/index.ts
+++ src/commands/bname/index.ts
@@ -1,28 +1,14 @@
-import { Args, Command, Flags } from '@oclif/core'
+import { Args } from '@oclif/core'
 
+import { JgCommand } from '../../jg-command.js'
 import utils from '../../utils/index.js'
 
-export default class Bname extends Command {
+export default class Bname extends JgCommand<typeof Bname> {
   static override args = {
     issueIdOrKey: Args.string({ description: 'Jira Issue ID or Key' }),
   }
 
   static override description = 'Generates a Git branch name from a Jira Issue ID/Key'
-
-  static override enableJsonFlag = true
-
-  static override examples = [
-    '<%= config.bin %> <%= command.id %>',
-    '<%= config.bin %> <%= command.id %> --clipboard',
-    '<%= config.bin %> <%= command.id %> --clipboard --quiet',
-    '<%= config.bin %> <%= command.id %> --json',
-  ]
-
-  static override flags = {
-    clipboard: Flags.boolean({ char: 'c', default: false, description: 'Copy to clipboard' }),
-    help: Flags.help({ char: 'h', description: 'Show help' }),
-    quiet: Flags.boolean({ char: 'q', dependsOn: ['clipboard'], description: 'Suppress output' }),
-  }
 
   public async run(): Promise<{ bname: string }> {
     const { args, flags } = await this.parse(Bname)
@@ -41,6 +27,8 @@ export default class Bname extends Command {
       .replaceAll(/\s/g, '-')
       .toLowerCase()
 
+    // TODO: Add support for just passing jira issue ID, not the entire key
+    // TODO: Limit length
     const message = `${type === 'Bug' ? 'fix' : 'feat'}/${issueKey}/${issueScope}-${issueSummary}`
     if (flags.clipboard) {
       utils.copyToClipboard(message)
