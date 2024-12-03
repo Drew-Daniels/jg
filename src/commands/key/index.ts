156bdfd449bc9790e6ee5e59435ca81a7f1e429d
Drew Daniels <drew.daniels@kipuhealth.com>
2024-12-02 19:02:28 -0600

    remove todo

--- src/commands/key/index.ts
+++ src/commands/key/index.ts
@@ -15,7 +15,6 @@ export default class Key extends Command {
     clipboard: Flags.boolean({ char: 'c', default: false, description: 'Copy to clipboard' }),
     help: Flags.help({ char: 'h', description: 'Show help' }),
     quiet: Flags.boolean({ char: 'q', description: 'Suppress output' }),
-    // TODO: Do I need to add a flag JSON or does setting enableJsonFlag in the top of the file do it?
   }
 
   public async run(): Promise<{ key: string }> {
