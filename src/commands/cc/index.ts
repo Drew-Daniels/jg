791e5ffbf4c0920833a3b962d5865c4b7dc07d23
Drew Daniels <drew.daniels@kipuhealth.com>
2024-12-02 20:47:26 -0600

    set cc command to inherit from JgCommand

--- src/commands/cc/index.ts
+++ src/commands/cc/index.ts
@@ -1,26 +1,15 @@
-import { Args, Command, Flags } from '@oclif/core'
+import { Args } from '@oclif/core'
 
+import { JgCommand } from '../../jg-command.js'
 import utils from '../../utils/index.js'
 
 // TODO: Rename this function so it doesn't share the same name as the C compiler, 'cc' command
-export default class Cc extends Command {
+export default class Cc extends JgCommand<typeof Cc> {
   static override args = {
     issueIdOrKey: Args.string({ description: 'Jira Issue ID or Key' }),
   }
 
   static override description = 'Generates a Conventional Commit Message from a Jira Issue ID/Key'
-
-  static override enableJsonFlag = true
-
-  static override examples = [
-    '<%= config.bin %> <%= command.id %>',
-  ]
-
-  static override flags = {
-    clipboard: Flags.boolean({ char: 'c', default: false, description: 'Copy to clipboard' }),
-    help: Flags.help({ char: 'h', description: 'Show help' }),
-    quiet: Flags.boolean({ char: 'q', dependsOn: ['clipboard'], description: 'Suppress output' }),
-  }
 
   public async run(): Promise<{ message: string }> {
     const { args, flags } = await this.parse(Cc)
