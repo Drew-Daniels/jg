6c1b045be4b04257ca9cf693cc1d448279e536d8
Drew Daniels <drew.daniels@kipuhealth.com>
2024-12-03 19:01:23 -0600

    add stubs for pr tests

--- test/commands/pr/index.test.ts
+++ test/commands/pr/index.test.ts
@@ -2,8 +2,23 @@ import { runCommand } from '@oclif/test'
 import { expect } from 'chai'
 
 describe('pr', () => {
-  it('runs pr cmd', async () => {
-    const { stdout } = await runCommand('pr')
-    expect(stdout).to.contain('hello world')
+  describe('when no arguments or flags are passed', () => {
+    describe('and on a Git branch with a Jira Issue key', () => {
+      describe('and no PR can be found for that Jira Issue key', () => {
+        it('throws an error')
+      })
+      describe('and a PR can be found for that Jira Issue key', () => {
+        it('logs a Slack message with a link to the Jira Issue and that PR')
+      })
+      describe('and more than one PR can be found for that Jira Issue key', () => {
+        it('logs a Slack message with a link to the Jira Issue and the most recent PR')
+      })
+    })
+    describe('and on a Git branch without a Jira Issue key in the name', () => {
+      it('throws an error')
+    })
+    describe('and not on a Git branch', () => {
+      it('throws an error')
+    })
   })
 })
